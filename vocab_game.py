3e18c688ce093d12acab66ec419a1a6532716020
412-28-paponwit <56422@yupparaj.ac.th>
2026-09-01 10:45:54 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -97,6 +97,8 @@ ans2 = st.text_input(
   #อัปเดตค่าล่าสุดเข้าตัวแปร
 st.session_state.ans1_val = ans1
 st.session_state.ans2_val = ans2
+st.session_state.ans3_val = ans3
+st.session_state.ans4_val = ans4
 
 ans3 = st.text_input(
     "ข้อ 3: I went to buy a `B _ _ _ pack`. 🎒",
