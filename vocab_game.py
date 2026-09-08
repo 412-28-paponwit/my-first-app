c0062671ccda5b4928d7c2daed9a8e275e91f2ee
412-28-paponwit <56422@yupparaj.ac.th>
2026-09-08 10:41:28 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -37,7 +37,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
     u_ans3 = ans3.strip().lower()
     u_ans4 = ans4.strip().lower()
     
-    # ตรวจข้อ 1
+    # ตรวจข้อ 1
     if u_ans1 == "apple":
         st.success("✅ ข้อ 1: ถูกต้อง")
         score += 1
