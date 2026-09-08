27762a4ec06fa3749c233e0fa8e1a09e376cacb1
412-28-paponwit <56422@yupparaj.ac.th>
2026-09-08 10:45:52 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -63,13 +63,13 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
         score += 1
     else:
         st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
-        
-    st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
+        
+    st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
 
-    if score == 4:
-        st.success("🎉 You win!")
-    else:
-        st.error("💀 You lose!")
+    if score == 4:
+        st.success("🎉 You win!")
+    else:
+        st.error("💀 You lose!")
 
 
 # ----------------------------------------------------
