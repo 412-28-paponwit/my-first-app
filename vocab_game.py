d706c7cd4b716294564e250a69cd8778d39c6e98
412-28-paponwit <56422@yupparaj.ac.th>
2026-09-08 10:48:22 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -92,19 +92,19 @@ st.divider()
 # 3. ช่องรับคำตอบ (ใช้ value ผูกกับตัวแปรตรงๆ เพื่อสั่งเคลียร์ได้)
 ans1 = st.text_input(
     "ข้อ 1: An `a _ _ l e` a day keeps the doctor away. 🍎",
-    value=st.session_state.ans1_val,
+     value=st.session_state.ans1_val,
 )
 ans2 = st.text_input(
-    "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
-    value=st.session_state.ans2_val,
+    "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
+     value=st.session_state.ans2_val,
 )
 ans3 = st.text_input(
-    "ข้อ 3: I grow `A _ _ _ ado`. 🥑",
-    value=st.session_state.ans3_val,
+    "ข้อ 3: I grow `A _ _ _ ado`. 🥑",
+     value=st.session_state.ans3_val,
 )
 ans4 = st.text_input(
-    "ข้อ 4: I like   `P_ _ ch`. 🍑",
-    value=st.session_state.ans4_val,
+    "ข้อ 4: I like   `P_ _ ch`. 🍑",
+    value=st.session_state.ans4_val,
 )
 # อัปเดตค่าล่าสุดเข้าตัวแปร
 st.session_state.ans1_val = ans1
